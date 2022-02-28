187d5654ba12d3e596cfa9e83c746abae0691b3c
Matheus Garcia <37114863+Krush206@users.noreply.github.com>
2022-02-28 11:28:28 -0300

    Add missing headers

---
+++ TurboC.h
@@ -0,0 +1,13 @@
+#include <sys/cdefs.h>
+#include <ncurses.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <sys/ioctl.h>
